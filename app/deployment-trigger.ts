eabe73125a1462877481d16a9d72ad9002b89acb
tc615guy <70850321+tc615guy@users.noreply.github.com>
2025-10-26 12:46:18 -0500

    🚀 Trigger Vercel deployment - AutoTaco

---
+++ app/deployment-trigger.ts
@@ -0,0 +1,5 @@
+// AutoTaco Deployment Trigger
+// This file was created to trigger a new Vercel deployment
+// Generated at: 2025-10-26T17:46:18.168Z
+
+export const deploymentTrigger = '4b2cdff6-6a1c-4d51-9eda-b7ed4e32f4c6'
